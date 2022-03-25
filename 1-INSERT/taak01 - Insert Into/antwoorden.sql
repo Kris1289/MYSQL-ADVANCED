481eac414e964f362db64d77245ed5d681508804
Kris <krdelver@gmail.com>
2022-03-25 12:53:58 +0100

    opdracht 1

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,18 @@
 
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (1, 'loka', 'nams', '9A', 'Albert Cuypstraat 3', '1234AB', '2001-04-28', 'Amstelveen');
+--------------------------------------------------------------------------------------------------
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (6, 'Mohammed', 'Atari', '9C', 'Kalverstraat 1', '1678DC', '2003-01-17', 'Amstelveen');
+---------------------------------------------------------------------------------------------------
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (5, 'Abdalla', 'Atari', '1C', 'Kalverstraat 2', '1278DC', '2004-02-18', 'Amstelveen');
+---------------------------------------------------------------------------------------------------
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (7, 'Karim', 'Atari', '8C', 'Kalverstraat 3', '1878DC', '2005-04-19', 'Amstelveen');
+--------------------------------------------------------------------------------------------------
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (9, 'peter', 'Atari', '7C', 'Kalverstraat 3', '1872DC', '2005-01-19', 'Amstelveen');
+-------------------------------------------------------------------------------------------------
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (10, 'jaren', 'Atari', '6C', 'Kalverstraat 8', '1478DC', '2005-01-19', 'Amstelveen');
