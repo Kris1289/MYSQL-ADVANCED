f7507da811b6f6fd0dce50f9eb87b54b2c9a1e02
Kris <krdelver@gmail.com>
2022-03-25 13:03:29 +0100

    opdracht 2

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,4 @@
-
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats)
+VALUES ('Mohamed','elhakeem','2001-03-2','Amsterdam'),
+('Ahmed','dago','1998-04-011', 'Amstelveen'),
+('Mustfa','danal', '2007-02-21', 'Rotterdam');
